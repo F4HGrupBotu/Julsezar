abeea0860969b90753fb48bf7d280d67eb264323
F4H Müzik Botu TR <sahipfh@gmail.com>
2024-06-13 02:38:51 +0200

    Update logger.py

--- AnieXEricaMusic/utils/logger.py
+++ AnieXEricaMusic/utils/logger.py
@@ -10,14 +10,14 @@ async def play_logs(message, streamtype):
         logger_text = f"""
 <b>{app.mention} ᴘʟᴀʏ ʟᴏɢ</b>
 ╔════❰𝐏𝐋𝐀𝐘𝐈𝐍𝐆❱═══❍⊱❁۪۪
-<b>◈ 𝐂𝐡𝐚𝐭 ➪ </b>{message.chat.title}
-<b>◈ 𝐂𝐡𝐚𝐭 𝐈𝐝 ➪ </b> <code>{message.chat.id}</code>
-<b>◈ 𝐔𝐬𝐞𝐫 ➪ </b> {message.from_user.mention}
-<b>◈ 𝐔𝐬𝐞𝐫𝐧𝐚𝐦𝐞 ➪ </b> @{message.from_user.username}
+<b>◈ 𝗦𝗢𝗛𝗕𝗘𝗧 ➪ </b>{message.chat.title}
+<b>◈ 𝗦𝗢𝗛𝗕𝗘𝗧 𝗜𝗗 ➪ </b> <code>{message.chat.id}</code>
+<b>◈ 𝗞𝗨𝗟𝗟𝗔𝗡𝗜𝗖𝗜 ➪ </b> {message.from_user.mention}
+<b>◈ 𝗞𝗨𝗟𝗟𝗔𝗡𝗜𝗖𝗜 𝗔𝗗𝗜 ➪ </b> @{message.from_user.username}
 <b>◈ 𝐈𝐝 ➪ </b> <code>{message.from_user.id}</code>
-<b>◈ 𝐂𝐡𝐚𝐭 𝐋𝐢𝐧𝐤 ➪ </b> @{message.chat.username}
-<b>◈ 𝐒𝐞𝐚𝐫𝐜𝐡𝐞𝐝 ➪ </b> <code>{message.text.split(None, 1)[1]}</code>
-<b>◈ 𝐁𝐲 ➪ </b> {streamtype}
+<b>◈ 𝗦𝗢𝗛𝗕𝗘𝗧 𝗟𝗜̇𝗡𝗞𝗜̇ ➪ </b> @{message.chat.username}
+<b>◈ 𝙰𝚁𝙰𝙽𝙳𝙸 ➪ </b> <code>{message.text.split(None, 1)[1]}</code>
+<b>◈ 𝗧𝗔𝗥𝗔𝗙𝗜𝗡𝗗𝗔𝗡 ➪ </b> {streamtype}
 ╚═══❰ #𝐍𝐞𝐰𝐒𝐨𝐧𝐠 ❱══❍⊱❁۪۪"""
         if message.chat.id != LOGGER_ID:
             try:
